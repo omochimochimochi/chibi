94aba40260f4e912b120ee1cc87035e3770ebb3f
NOMURA Ayano <21716071@jwu.ac.jp>
2019-10-07 15:46:19 +0900

    counter class

--- calc.py
+++ calc.py
@@ -1,5 +1,10 @@
 
 def calc(s):
-    return int(s)
+    print('s=', s)
+    nums = map(int, s.split('+'))
+    #print('nums=', nums)
+    return sum(nums)
 
 print(calc("1"))
+print(calc("1+2"))
+print(calc("1+2+3"))

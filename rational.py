5f0df8f8e5eb2b238981624d6c4ad49175df8ef6
NOMURA Ayano <21716071@jwu.ac.jp>
2019-10-14 15:44:27 +0900

    Val

--- rational.py
+++ rational.py
@@ -1,12 +1,14 @@
+import math
+
 class Q (object):
-    def __init__(self, a,b):
-        self.a = a
-        self.b = b
+    def __init__(self, a,b=1):
+        gcd = math.gcd(a,b)
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self):
         if self.b == 1:
             return str(self.a)
         return f'{self.a}/{self.b}'
-
     def __add__(self, q):
         a = self.a
         b = self.b

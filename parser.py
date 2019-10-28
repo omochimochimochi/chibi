2f7e862b4b0f508c2f4308963698635934b58064
NOMURA Ayano <21716071@jwu.ac.jp>
2019-10-28 16:37:53 +0900

    output Val

--- parser.py
+++ parser.py
@@ -1,33 +1,18 @@
 from exp import Val, Add, Sub, Mul, Div
 
 def parse(s: str):
-    if pos == -1:
-        return val(int(s))
-    else:
-        s1 = s[0:pos]
-        s2 = s[pos+1:]
-        return Add (parse(s1), parse(s2))
-
-e = parse("123")
-print(e.eval())
-
-s = "123456"
-pos = s.find("+")　# + 記号を探す
-print('pos', pos)
-
-s1 = s[0:pos]
-s2 = s[pos+1:]
-print(s, s1, s2)   # + 記号で分割
-
-
-def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        return Val(int(s))
-    else:
+    if s find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
+    if s.find('*')  > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+    return Val(int(s))
 
-    e = parse("1+2+3")
-    print(e, e.eval())
+e = parse("1-2-3")
+print(e, e.eval())
+assert e.eval() == -4
